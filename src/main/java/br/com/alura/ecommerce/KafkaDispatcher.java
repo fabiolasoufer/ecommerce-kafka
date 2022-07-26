f589c6922a202f4a07b8c674ca81b24f48cb09f2
Fabiola <fabiolasou.za@live.com>
2022-07-25 22:10:58 -0300

    first commit

---
+++ src/main/java/br/com/alura/ecommerce/KafkaDispatcher.java
@@ -0,0 +1,46 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.io.Closeable;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+class KafkaDispatcher implements Closeable {
+
+    private final KafkaProducer<String, String> producer;
+
+    KafkaDispatcher() {
+        this.producer = new KafkaProducer<>(properties());
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        return properties;
+    }
+
+    void send(String topic, String key, String value) throws ExecutionException, InterruptedException {
+        var record = new ProducerRecord<>(topic, key, value);
+        Callback callback = (data, ex) -> {
+            if (ex != null) {
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println("Success sending " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
+        };
+        producer.send(record, callback).get();
+    }
+
+    @Override
+    public void close() {
+        producer.close();
+    }
+
+}
